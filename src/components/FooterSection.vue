386948363445ff8db9d59d84dade1e35985c907c
Alexander <shymbabigmir@gmail.com>
2023-01-24 12:38:18 +0200

    Added footer section and styling it

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="footer">
-    <h1>Footer Block</h1>
+    <div class="footer-section">
+      <div class="footer-contacts">
+        <address class="contacts">
+          <a href="mailto:philipp@funtcoffee.com.ua">
+            <span class="email">e-mail: philipp@funtcoffee.com.ua</span>
+          </a>
+          <a class="tel:+380970010508">
+            <span>tel: +38 097 001 05 08</span>
+          </a>
+        </address>
+      </div>
+      <div class="footer-social">
+        <ul class="social">
+          <li class="social-item fb"><a href="#" class="social-link">f</a></li>
+          <li class="social-item in"><a href="#" class="social-link">In</a></li>
+        </ul>
+      </div>
+    </div>
   </div>
 
 </template>
@@ -11,6 +28,61 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 4rem;
+  color: #FFFFFF;
+
+  .footer-section {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding-top: 20px;
+    padding-left: 25px;
+
+    .contacts {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+
+      a {
+        text-decoration: none;
+        color: #FFFFFF;
+      }
+    }
+
+    .footer-social {
+      margin-right: 20px;
+    }
+
+    .social {
+      padding: 0;
+      list-style: none;
+      display: flex;
+    }
+
+    .social-item {
+      width: 36px;
+      height: 36px;
+      background-color: #5b5249;
+      color: #FFFFFF;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border-radius: 50%;
+      margin-left: 10px;
+    }
+
+    .social-link {
+      text-decoration: none;
+      font-size: 16px;
+      color: #fff;
+    }
+
+  }
+}
 
 </style>
--- src/components/MainSection.vue
+++ src/components/MainSection.vue
@@ -47,7 +47,7 @@ export default {
   flex-wrap: wrap;
   align-items: center;
   justify-content: center;
-
+  padding-bottom: 4rem;
 }
 
 
