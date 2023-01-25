e33f46ffb11c76ebdf35195e72a9b34a4f339182
Alexander <shymbabigmir@gmail.com>
2023-01-25 16:27:18 +0200

    Refactoring, second part

--- src/modules/apiService.js
+++ src/modules/apiService.js
@@ -2,7 +2,13 @@ export default class ApiService {
 
     async getAllProducts() {
         const urlBase = 'https://fakestoreapi.com/products';
-        const response = await fetch(urlBase).then((res) => (res.json()));
-        return response
+        const response_products = await fetch(urlBase).then((res) => (res.json()));
+        return response_products
+    }
+
+    async getSelectedCategory(category) {
+        const urlBase = `https://fakestoreapi.com/products/category/${category}`;
+        const response_category = await fetch(urlBase).then((res) => (res.json()));
+        return response_category
     }
 }
