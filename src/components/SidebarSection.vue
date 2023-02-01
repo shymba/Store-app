56631baf619117340d31e351e7db349455391c0d
Alexander <shymbabigmir@gmail.com>
2023-02-01 15:41:47 +0200

    Added login page with form inputs for user

--- src/components/SidebarSection.vue
+++ src/components/SidebarSection.vue
@@ -2,6 +2,10 @@
   <div class="sidebar" v-if="show">
     <div class="sidebar-menu">
       <div class="close-sidebar" @click.stop="hideSidebar">X</div>
+      <div class="sidebar-login" @click="showLogin">
+        <p>LOGIN</p>
+        <img src="../assets/icon-login.png"/>
+      </div>
       <div class="menu-content">
         <div
             class="category-menu"
@@ -13,15 +17,18 @@
         </div>
       </div>
     </div>
+    <LoginPage v-model:showLoginPage="loginPageVisible"/>
   </div>
 </template>
 
 <script>
 import ApiService from "@/modules/apiService";
+import LoginPage from "@/components/LoginPage";
 
 const apiListCategory = new ApiService();
 export default {
   name: "SidebarSection",
+  components: {LoginPage},
   props: {
     show: {
       type: Boolean,
@@ -31,6 +38,7 @@ export default {
   data() {
     return {
       categoryProducts: null,
+      loginPageVisible:false
     }
   },
   methods: {
@@ -43,6 +51,9 @@ export default {
     },
     hideSidebar() {
       this.$emit('update:show', false)
+    },
+    showLogin() {
+      this.loginPageVisible = true;
     }
   },
   mounted() {
@@ -67,6 +78,14 @@ export default {
     font-weight: bold;
     cursor: pointer;
     margin: 10px;
+  }
+
+  .sidebar-login {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #006000;
+    cursor: pointer;
   }
 
   .menu-content {
