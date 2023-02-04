47a83739cf6caf7ef5337dd461d7d407b24f0809
Alexander <shymbabigmir@gmail.com>
2023-02-04 21:02:00 +0200

    Third part of update

--- src/components/SidebarSection.vue
+++ src/components/SidebarSection.vue
@@ -2,17 +2,7 @@
   <div class="sidebar" v-if="show">
     <div class="sidebar-menu">
       <div class="close-sidebar" @click.stop="hideSidebar">X</div>
-      <div class="sidebar-login" @click="loginUser" v-if="!firstname">
-        <p>LOGIN</p>
-        <img src="../assets/icon-login.png"/>
-      </div>
-      <div class="sidebar-login" v-else @click="logoutUser">
-        <div class="welcome-user">
-          Welcome, {{firstname}}<br>{{lastname}}!
-        </div>
-        <p>LOGOUT</p>
-        <img src="../assets/icons-logout.png"/>
-      </div>
+      <LoginUser />
       <div class="menu-content">
         <div
             class="category-menu"
@@ -24,21 +14,17 @@
         </div>
       </div>
     </div>
-    <LoginPage
-        v-model:showLoginPage="loginPageVisible"
-        @loggedUser="getUser"
-    />
   </div>
 </template>
 
 <script>
 import ApiService from "@/modules/apiService";
-import LoginPage from "@/components/LoginPage";
+import LoginUser from "@/components/LoginUser";
 
 const apiListCategory = new ApiService();
 export default {
   name: "SidebarSection",
-  components: {LoginPage},
+  components: {LoginUser},
   props: {
     show: {
       type: Boolean,
@@ -65,18 +51,6 @@ export default {
     hideSidebar() {
       this.$emit('update:show', false)
     },
-    loginUser() {
-      this.loginPageVisible = true;
-    },
-    logoutUser() {
-      this.firstname = '';
-      this.lastname = '';
-    },
-    async getUser(id) {
-      const user = await apiListCategory.getOneUser(id);
-      this.firstname = user.name.firstname
-      this.lastname = user.name.lastname
-    }
   },
   mounted() {
     this.getAllCategory();
@@ -100,22 +74,6 @@ export default {
     font-weight: bold;
     cursor: pointer;
     margin: 10px;
-  }
-
-  .sidebar-login {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: #006000;
-    cursor: pointer;
-
-    .welcome-user {
-      font-size: 12px;
-      margin-right: 10px;
-      text-align: start;
-      letter-spacing: 2px;
-      text-transform: capitalize;
-    }
   }
 
   .menu-content {
